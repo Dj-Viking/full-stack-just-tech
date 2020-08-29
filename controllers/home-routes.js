1744b45e16822c68664170614c4f94c93b5f4336
DJ Viking <anders.swedishviking@gmail.com>
2020-08-29 02:25:27 -0400

    able to see all posts on the home page

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,19 +1,80 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection.js');
+const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-  res.render('homepage', 
+  Post.findAll(
     {
-      id: 1,
-      post_url: 'https://handlebars.js.com/guide/',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      vote_count: 10,
-      comments: [{}, {}],
-      user: {
-        username: 'test-user'
-      }
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [
+          sequelize.literal(
+            '(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'
+          ), 'vote_count'
+        ]
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at'
+          ],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
     }
-  );
-});
+  )
+  .then(dbPostData => {
+    console.log(`
+    `);
+    console.log("\x1b[33m", "testing homepage render", "\x1b[00m");
+    console.log(`
+    `);
+    //console.log(dbPostData[0]);
+    //pass a single post object into the homepage template
+    //   res.render('homepage', 
+    //     dbPostData[0].get(
+    //       { 
+    //         plain: true 
+    //       }
+    //     )
+    //   );
+    // })
 
+  
+    // res.render('homepage', 
+    //   {
+      //     id: 1,
+      //     post_url: 'https://handlebars.js.com/guide/',
+      //     title: 'Handlebars Docs',
+      //     created_at: new Date(),
+      //     vote_count: 10,
+      //     comments: [{}, {}],
+      //     user: {
+        //       username: 'test-user'
+        //     }
+        //   }
+        // );
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', { posts });
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+})
 module.exports = router;
